ab481828e5cd210a1d0f09d9cc58b46e2b451dc1
Ademilson-Chagas-Cappellari <adechagas@gmail.com>
2022-11-17 01:07:51 -0300

    Projeto criado

---
+++ backend/src/test/java/com/ademilson/metasvendas/MetasvendasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ademilson.metasvendas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MetasvendasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
